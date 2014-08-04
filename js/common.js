356b4d247de994998dd54489f52023eac5775627
moroz <morozarturv@gmail.com>
2014-08-04 17:54:51 +0300

    added news and about pages

--- js/common.js
+++ js/common.js
@@ -1,8 +1,22 @@
 head.ready(function() {
 
-	// $(document).on("click", function(){
-	// 	$(".js-popup").hide();
-	// });
+	// order
+	var order = $('.js-order'),
+			order_trigger = $('.js-order-trigger');
+	order_trigger.on('click', function () {
+		order_trigger.addClass('is-active');
+		order.show();
+		return false;
+	});
 
-	console.log($('body').html());
+	// about slider
+	var about_slider = $('.js-about-slider');
+	if (about_slider.length) {
+		about_slider.slick({
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			dots: true
+		});
+	};
+
 });
